9763d0d145b34b03f2c217240f457bd4bec66b33
ZacharyCampanelli <zaccamp@optonline.net>
2025-10-17 19:24:10 -0400

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+import bgMobile from './assets/images/pattern-bg-mobile.png'
+
+function App() {
+
+  return (
+    <>
+  <div>
+    <span className='background_image'/>
+  </div>
+    </>
+  )
+}
+
+export default App
