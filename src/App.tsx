4a3e83beaf7d7462a045ee338594736c2e3096c9
Zachary Campanelli <zackjcampanelli@gmail.com>
2025-10-24 21:45:50 -0400

    Revert "Added header image as well as Storybook"
    This reverts commit 8973de8c743e556ac2b63413e6b3ca56026866f8.

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,9 @@
 import './App.css'
 
-import { useEffect, useState } from 'react'
-
 import bgMobile from './assets/images/pattern-bg-mobile.png'
 
 function App() {
-const [IpAddress, setIpAddress] = useState('')
 
-useEffect(() => {
-  const fetchIp = async () => {
-    try{
-      const url = `https://geo.ipify.org/api/v2/country?apiKey=${import.meta.env.IPIFY_API_KEY}${IpAddress ? `&ipAddress=${IpAddress}` : ''}`
-      const response = await fetch(url)
-    }
-  }
-}, [])
   return (
     <>
   <div>
