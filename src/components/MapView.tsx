0c20d53752ce88459623d58c2ca6cd6f7caeb888
Zachary Campanelli <zackjcampanelli@gmail.com>
2025-11-15 20:19:18 -0500

    Add Marker component to MapView

--- src/components/MapView.tsx
+++ src/components/MapView.tsx
@@ -1,4 +1,7 @@
-import { Map } from "@vis.gl/react-google-maps"
+import {
+ Map,
+ Marker
+} from "@vis.gl/react-google-maps"
 
 type MapViewProps = {
   coordinates: { lat: number; lng: number };
@@ -7,9 +10,11 @@ type MapViewProps = {
 const MapView = ({ coordinates }: MapViewProps) => {
   return (
     <div id="map">
-    <Map defaultZoom={13} center={{ lat: coordinates.lat, lng: coordinates.lng }} disableDefaultUI={true} id="map" />  
+      <Map defaultZoom={13} center={{ lat: coordinates.lat, lng: coordinates.lng }} disableDefaultUI={true} id="map">
+        <Marker position={{ lat: coordinates.lat, lng: coordinates.lng }} icon={{url:"/icon-location.svg", scaledSize: { width: 48, height:64 } }} />
+      </Map>
     </div>
   )
-}
+}    
 
 export default MapView
